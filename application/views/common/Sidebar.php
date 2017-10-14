f043a5cf6bb62f2b66e507e2b58509e6acd92f92
dipankar <dipankarnath008@gmail.com>
2017-10-14 09:42:28 +0530

    unnessesary menus removed from sidebar

--- application/views/common/Sidebar.php
+++ application/views/common/Sidebar.php
@@ -10,21 +10,6 @@
                     <h6>Victoria Baker</h6>
                     <span class="text-size-small">Santa Ana, CA</span>
                 </div>
-
-                <div class="sidebar-user-material-menu">
-                    <a href="#user-nav" data-toggle="collapse"><span>My account</span> <i class="caret"></i></a>
-                </div>
-            </div>
-
-            <div class="navigation-wrapper collapse" id="user-nav">
-                <ul class="navigation">
-                    <li><a href="#"><i class="icon-user-plus"></i> <span>My profile</span></a></li>
-                    <li><a href="#"><i class="icon-coins"></i> <span>My balance</span></a></li>
-                    <li><a href="#"><i class="icon-comment-discussion"></i> <span><span class="badge bg-teal-400 pull-right">58</span> Messages</span></a></li>
-                    <li class="divider"></li>
-                    <li><a href="#"><i class="icon-cog5"></i> <span>Account settings</span></a></li>
-                    <li><a href="#"><i class="icon-switch2"></i> <span>Logout</span></a></li>
-                </ul>
             </div>
         </div>
         <!-- /user menu -->
@@ -37,22 +22,7 @@
 
                     <!-- Main -->
                     <li class="navigation-header"><span>Main</span> <i class="icon-menu" title="Main pages"></i></li>
-                    <li class="active"><a href="index.html"><i class="icon-home4"></i> <span>Dashboard</span></a></li>
-                    <li>
-                        <a href="#"><i class="icon-stack2"></i> <span>Page layouts</span></a>
-                        <ul>
-                            <li><a href="layout_navbar_fixed.html">Fixed navbar</a></li>
-                            <li><a href="layout_navbar_sidebar_fixed.html">Fixed navbar &amp; sidebar</a></li>
-                            <li><a href="layout_sidebar_fixed_native.html">Fixed sidebar native scroll</a></li>
-                            <li><a href="layout_navbar_hideable.html">Hideable navbar</a></li>
-                            <li><a href="layout_navbar_hideable_sidebar.html">Hideable &amp; fixed sidebar</a></li>
-                            <li><a href="layout_footer_fixed.html">Fixed footer</a></li>
-                            <li class="navigation-divider"></li>
-                            <li><a href="boxed_default.html">Boxed with default sidebar</a></li>
-                            <li><a href="boxed_mini.html">Boxed with mini sidebar</a></li>
-                            <li><a href="boxed_full.html">Boxed full width</a></li>
-                        </ul>
-                    </li>
+                    <li class="active"><a href="#"><i class="icon-home4"></i> <span>Dashboard</span></a></li>
                     <li><a href="<?php echo base_url()?>Dashboard/page/AddVehicle"><i class="icon-home4"></i> <span>Add Vehicle</span></a></li>
                     <li><a href="<?php echo base_url()?>Dashboard/view"><i class="icon-home4"></i> <span>View Vehicle</span></a></li>
 
